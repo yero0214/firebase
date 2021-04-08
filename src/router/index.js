93f41c55a1903ab28485c86be8f30d0492b56fc5
yero0214 <yero0214@gmail.com>
2021-04-08 18:13:23 +0900

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Login from '../views/Login.vue'
+import SignUp from '../views/Sign-up.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,12 +12,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
   }
 ]
 
--- src/views/Sign-up.vue
+++ src/views/Sign-up.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    Home
+  <div class="signup">
+    SignUp
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
+  name: 'SignUp',
   components: {
 
   }
